b4772a620760515eb979936deb87a54f434f6851
Kervon Ryan <kervonryan96@gmail.com>
2022-01-31 19:30:09 -0400

    Update main.tf
    Changed jwt from using manual key to url

--- main.tf
+++ main.tf
@@ -300,7 +300,7 @@ locals {
     },
     {
       name  = "HASURA_GRAPHQL_JWT_SECRET",
-      value = "{\"type\":\"${var.hasura_jwt_secret_algo}\", \"key\": \"${var.hasura_jwt_secret_key}\"}"
+      value = "{\"jwk_url\":\"${var.hasura_jwt_secret_url}\"}"
     }
   ]
 
